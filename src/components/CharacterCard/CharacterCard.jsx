09e08decdb468d5abf54c4f2dabb10872ca205f3
Edgar Rivera <edgar.riveraj@gmail.com>
2023-03-20 00:51:27 -0600

    performance

--- src/components/CharacterCard/CharacterCard.jsx
+++ src/components/CharacterCard/CharacterCard.jsx
@@ -1,47 +1,51 @@
 import { useState, useEffect } from "react";
+import { LazyLoadImage } from "react-lazy-load-image-component";
 import Bow from "../../assets/images/Bow.webp";
 import Catalyst from "../../assets/images/Catalyst.webp";
 import Claymore from "../../assets/images/Claymore.webp";
 import Polearm from "../../assets/images/Polearm.webp";
 import Sword from "../../assets/images/Sword.webp";
+import "react-lazy-load-image-component/src/effects/blur.css";
+import "react-lazy-load-image-component/src/effects/black-and-white.css";
 import styles from './CharacterCard.module.css'
 
 const CharacterCard = ({ info, character, onHandle }) => {
     const [characterInfo, setCharacterInfo] = useState();
-    const [image, setImage] = useState();
+    const [isLoading, setIsLoading] = useState(true);
+    const [weaponImage, setWeaponImage] = useState()
 
     async function getCharacterInfo() {
         const response = await info;
         setCharacterInfo(response);
+        setIsLoading(false)
     }
 
-    const weaponImage = [
-        <img className={styles.imageWeapon} src={Bow} alt={Bow} />,
-        <img className={styles.imageWeapon} src={Catalyst} alt={Catalyst} />,
-        <img className={styles.imageWeapon} src={Claymore} alt={Claymore} />,
-        <img className={styles.imageWeapon} src={Polearm} alt={Polearm} />,
-        <img className={styles.imageWeapon} src={Sword} alt={Sword} />,
-    ];
-
-    const arrayWeapons = ["Bow", "Catalyst", "Claymore", "Polearm", "Sword"];
-
-    const index = arrayWeapons.indexOf(characterInfo?.weapon);
+    const weapons = {
+        'Bow': Bow,
+        'Catalyst': Catalyst,
+        'Claymore': Claymore,
+        'Polearm': Polearm,
+        'Sword': Sword,
+    }
 
     const handleClick = () => {
         onHandle(character);
     }
-
+    
     useEffect(() => {
         if (characterInfo?.weapon) {
-            setImage(weaponImage[index]);
+            setWeaponImage(weapons[characterInfo?.weapon]);
         }
-    }, [characterInfo?.weapon]);
+    }, [characterInfo]);
 
     useEffect(() => {
-        getCharacterInfo();
-    }, []);
+        if (info) {
+            setIsLoading(true);
+            getCharacterInfo();
+        }
+    }, [info]);
 
-    if (!characterInfo) {
+    if (isLoading) {
         return (
             <div className={styles.loader_container}>
                 <div className={styles.loader}></div>
@@ -53,23 +57,33 @@ const CharacterCard = ({ info, character, onHandle }) => {
         <div className={styles.card_container} onClick={handleClick}>
             <header className={styles.header}>{characterInfo.name}</header>
             <main className={styles.main}>
-                <img
+                <LazyLoadImage
                     className={styles.image}
+                    alt={`${character} card image`}
                     src={`https://api.genshin.dev/characters/${character}/card`}
-                    alt="image card character"
+                    effect="blur"
                 />
             </main>
             <footer className={styles.footer}>
                 <aside>
-                    <img className={styles.imageElement}
+                    <LazyLoadImage
+                        className={styles.imageElement}
                         src={`https://api.genshin.dev/elements/${characterInfo.vision.toLowerCase()}/icon`}
-                        alt="element icon"
+                        alt={`${characterInfo.vision} element icon`}
+                        effect="black-and-white"
                     />
                 </aside>
                 <aside>
                     <p className={styles.nation}>{characterInfo.nation}</p>
                 </aside>
-                {image}
+                <aside>
+                    <LazyLoadImage
+                        className={styles.imageWeapon}
+                        src={weaponImage}
+                        alt={`${characterInfo.weapon} weapon`}
+                        effect="black-and-white"
+                    />
+                </aside>
             </footer>
         </div>
     );
